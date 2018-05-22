2e05100377b95a5d96dbded88f805d5643a75336
Sagar Chamling <sagarchamling@lftechnology.com>
2018-05-22 14:15:12 +0545

    Add util function to generate unique token

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -8,3 +8,14 @@ import { v4 } from 'uuid';
 export function generate(): string {
   return v4();
 }
+
+/**
+ * Generate random alphanumeric string without '-' symbol.
+ *
+ * @returns {string}
+ */
+export function generateToken(): string {
+  const token = `${v4()}${v4()}`;
+
+  return token.replace(/-/g, '');
+}
