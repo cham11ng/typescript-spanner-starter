9c82eb53d28cc007ad05a1fa5614bcba52782bc0
Sagar Chamling <sagarchamling@lftechnology.com>
2018-05-22 12:31:23 +0545

    Add acl middleware in routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
+import acl from './middlewares/acl';
 import * as validate from './middlewares/validate';
 import * as homeController from './controllers/home';
 import * as userController from './controllers/user';
 import * as authController from './controllers/auth';
+import authenticate from './middlewares/authenticate';
 import { loginSchema } from './validators/loginRequest';
-import validateRefreshToken from './middlewares/validateRefreshToken';
 import { userPOSTSchema } from './validators/userRequest';
+import validateRefreshToken from './middlewares/validateRefreshToken';
 
 const router: Router = Router();
 
@@ -16,7 +18,7 @@ router.post('/login', validate.schema(loginSchema), authController.login);
 router.post('/refresh', validateRefreshToken, authController.refresh);
 router.post('/logout', validateRefreshToken, authController.logout);
 
-router.get('/users', userController.index);
-router.post('/users', validate.schema(userPOSTSchema), userController.store);
+router.get('/users', authenticate, acl, userController.index);
+router.post('/users', authenticate, acl, validate.schema(userPOSTSchema), userController.store);
 
 export default router;
